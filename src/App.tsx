828a141fe0ccf26c85ff926cc6753b08f388d116
Anay Sharma <anaysharma8791@gmail.com>
2022-11-16 12:50:24 +0530

    added logic to note form

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NewNote } from './NewNote';
 import { Container } from 'react-bootstrap';
 
+export type note = {
+  id: string;
+} & NoteData;
+
+export type RawNote = {
+  id: string;
+};
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: tag[];
+};
+
+export type tag = {
+  id: string;
+  label: string;
+};
+
 function App() {
+  const [notes, setNoted] = useLocalStorage<RawNote[]>('NOTES', []);
+  const [tags, setTags] = useLocalStorage<tag[]>('TAGS', []);
+
   return (
     <Container className="my-4">
       <Routes>
