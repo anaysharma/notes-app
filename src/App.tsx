66d632a8da6b5888d8995b1ef4bbcc2c3aee4e12
Anay Sharma <anaysharma8791@gmail.com>
2022-11-16 13:01:37 +0530

    added logic to store note data

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NewNote } from './NewNote';
 import { Container } from 'react-bootstrap';
+import { useLocalStorage } from './useLocalStorage';
+import { useMemo } from 'react';
 
 export type note = {
   id: string;
@@ -9,7 +11,7 @@ export type note = {
 
 export type RawNote = {
   id: string;
-};
+} & RawNoteData;
 
 export type RawNoteData = {
   title: string;
@@ -31,6 +33,14 @@ export type tag = {
 function App() {
   const [notes, setNoted] = useLocalStorage<RawNote[]>('NOTES', []);
   const [tags, setTags] = useLocalStorage<tag[]>('TAGS', []);
+  const noteWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
 
   return (
     <Container className="my-4">
