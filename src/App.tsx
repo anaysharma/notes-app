8bbe8868519abb45eaee472dd849548340d5c458
Anay Sharma <anaysharma8791@gmail.com>
2022-11-16 19:27:20 +0530

    added note editing functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { v4 as uuidV4 } from 'uuid';
 import { NoteList } from './NoteList';
 import { NoteLayout } from './NoteLayout';
 import { Note } from './Note';
+import { EditNote } from './EditNote';
 
 export type Note = {
   id: string;
@@ -55,6 +56,18 @@ function App() {
     });
   }
 
+  function onUpdateNote(id: string, { tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return prevNotes.map((note) => {
+        if (note.id === id) {
+          return { ...note, ...data, tagIds: tags.map((tag) => tag.id) };
+        } else {
+          return note;
+        }
+      });
+    });
+  }
+
   function AddTag(tag: Tag) {
     setTags((prev) => [...prev, tag]);
   }
@@ -78,7 +91,16 @@ function App() {
         />
         <Route path="/:id" element={<NoteLayout notes={noteWithTags} />}>
           <Route index element={<Note />} />
-          <Route path="edit" element={<h1>edit</h1>} />
+          <Route
+            path="edit"
+            element={
+              <EditNote
+                onSubmit={onUpdateNote}
+                onAddTag={AddTag}
+                availableTags={tags}
+              />
+            }
+          />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
