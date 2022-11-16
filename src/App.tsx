c7fb142d09eb282d4e7b4101766f6d7159df960a
Anay Sharma <anaysharma8791@gmail.com>
2022-11-16 12:26:15 +0530

    added new note form jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NewNote } from './NewNote';
+import { Container } from 'react-bootstrap';
+
 function App() {
   return (
-    <div>hi</div>
-  )
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Show</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
